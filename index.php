b1cb98918091cd8f20ebf23b05f447c1ff14d1c6
David Juarez <davidjuarez1411@gmail.com>
2018-07-24 15:51:17 -0700

    added html and css for half of the homepage for mobile devices

--- index.php
+++ index.php
@@ -3,34 +3,39 @@
 	<head>
 		<meta charset="utf-8">
 		<title>Perslice Homepage</title>
-		<link rel="stylesheet" href="css/style.css">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/style.css">
 	</head>
 	<body>
-		<nav class="navbar navbar-inverse">
-			<div class="container-fluid">
-				<a href="#">CALL NOW : (888) 567-1901</a>
+		<div id="banner" class="text-center">
+			<p>SIGN UP WITH YOUR EMAIL TO RECEIVE YOUR PROMO CODE NOW</p>
+		</div>
+		<nav class="navbar">
+			<div class="container">
+				<a id="number" href="#">CALL NOW : (888) 567-1901</a>
 				<div class="navbar-header">
 					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
 						<span class="icon-bar"></span>
 						<span class="icon-bar"></span>
 						<span class="icon-bar"></span>
 					</button>
-					<a class="navbar-brand" href="#">Perslice</a>
+					<a class="navbar-brand" href="#">
+						<img src="img/Logo_Perslice.png" alt="perslice logo">
+					</a>
 				</div>
 				<div class="collapse navbar-collapse" id="myNavbar">
 					<ul class="nav navbar-nav">
 						<li class="active"><a href="#">HOME</a></li>
 						<li><a href="#">SHOP</a></li>
 						<li class="dropdown">
-							<a class="dropdown-toggle" data-toggle="dropdown" href="#">CONTACT<span class="caret"></span></a>
+							<a class="dropdown-toggle" data-toggle="dropdown" href="#">CONTACT <span class="caret"></span></a>
 							<ul class="dropdown-menu">
 							<li><a href="#">RETAILER</a></li>
 							<li><a href="#">DISTRIBUTOR</a></li>
 							</ul>
 						</li>
 						<li class="dropdown">
-							<a class="dropdown-toggle" data-toggle="dropdown" href="#">BLOG<span class="caret"></span></a>
+							<a class="dropdown-toggle" data-toggle="dropdown" href="#">BLOG <span class="caret"></span></a>
 							<ul class="dropdown-menu">
 							<li><a href="#">RECIPES</a></li>
 							<li><a href="#">SPECIAL EVENTS</a></li>
@@ -45,6 +50,80 @@
 				</div>
 			</div>
 		</nav>
+		<section id="landing">
+			<div class="container">
+				<div class="row">
+					<div id="landingImg" class="col-md-8">
+						<img src="img/slicedCake.jpg" alt="sliced cake">
+					</div>
+					<div id="landingCta" class="col-md-4">
+						<h1>PERSLICE</h1>
+						<p>Spend less time slicing<br> and more time celebrating</p>
+						<a class="ctaBtn" href="#">Shop Now</a>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section id="shopHm">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-4">
+						<img src="img/Perslice.jpg" alt="perslice cake slicer and server">
+						<h2 class="productTitle">Perslice<br> Cake Slicer and Server</h2>
+						<p id="productPrice">$24.99</p>
+						<a class="ctaBtn" href="#">Quick View</a>
+					</div>
+					<div class="col-md-4">
+						<img src="img/cakeStand.jpg" alt="perslice cake slicer and server">
+						<h2 class="productTitle">Perslice<br> Cake Stand</h2>
+						<p id="productPrice">$24.99</p>
+						<a class="ctaBtn" href="#">Quick View</a>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section id="specials">
+			<div id="myCarousel" class="carousel slide" data-ride="carousel">
+			  <!-- Indicators -->
+			  <!-- <ol class="carousel-indicators">
+			    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+			    <li data-target="#myCarousel" data-slide-to="1"></li>
+			  </ol> -->
+
+			  <!-- Wrapper for slides -->
+			  <div class="carousel-inner">
+			    <div class="item active">
+			      <img src="img/cakeHeart.jpg" alt="cake with heart">
+				  <h3>Order 2<br> <span>save 20%</span></h3>
+				  <p>For a limited time we are offering an additional 20% off when you purchase one for you and one for a friend or family member</p>
+				  <a class="ctaBtn" href="#">Shop Now</a>
+			    </div>
+			    <div class="item">
+			      <img src="img/babyBitesCake.jpg" alt="baby bites cake">
+				  <h3><span>Enter to win</span> <br>$500</h3>
+				  <p>All you have to do is take a photo and send it to us and tell us about your Perslice experience</p>
+				  <a class="ctaBtn" href="#">Learn More</a>
+			    </div>
+			  </div>
+			  <!-- Left and right controls -->
+			  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+			    <span class="glyphicon glyphicon-chevron-left"></span>
+			    <span class="sr-only">Previous</span>
+			  </a>
+			  <a class="right carousel-control" href="#myCarousel" data-slide="next">
+			    <span class="glyphicon glyphicon-chevron-right"></span>
+			    <span class="sr-only">Next</span>
+			  </a>
+			</div>
+		</section>
+		<section id="newsletter">
+			<div class="container-fluid">
+				<div id="cakepops">
+					<h3>Great Offers &amp; Free Recipes</h3>
+					<a class="ctaBtn" href="#">Sign Up For Our Newsletter</a>
+				</div>
+			</div>
+		</section>
 	</body>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
