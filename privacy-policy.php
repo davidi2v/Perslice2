e6ab076677355618d84bf6fa0431cd5571aeebe9
David Juarez <davidjuarez1411@gmail.com>
2018-07-27 12:31:58 -0700

    added privacy policy and terms of service pages

---
+++ privacy-policy.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Perslice Privacy Policy</title>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/style.css">
+	</head>
+	<?php include 'banner.php'?>
+	<nav class="navbar">
+		<div class="container">
+			<a id="number" href="#">CALL NOW : (888) 567-1901</a>
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="#">
+					<img src="img/Logo_Perslice.png" alt="perslice logo">
+				</a>
+			</div>
+			<div class="collapse navbar-collapse" id="myNavbar">
+				<ul class="nav navbar-nav">
+					<li><a href="#">HOME</a></li>
+					<li class="active"><a href="shop.php">SHOP</a></li>
+					<li class="dropdown">
+						<a class="dropdown-toggle" data-toggle="dropdown" href="#">CONTACT <span class="caret"></span></a>
+						<ul class="dropdown-menu">
+						<li><a href="#">RETAILER</a></li>
+						<li><a href="#">DISTRIBUTOR</a></li>
+						</ul>
+					</li>
+					<li class="dropdown">
+						<a class="dropdown-toggle" data-toggle="dropdown" href="blogs.php">ARTICLES<span class="caret"></span></a>
+						<ul class="dropdown-menu">
+						<li><a href="blogs.php">BLOGS</a></li>
+						<li><a href="recipes-all.php">RECIPES</a></li>
+						<li><a href="#">SPECIAL EVENTS</a></li>
+						</ul>
+					</li>
+					<li><a href="#">HOW TO USE</a></li>
+				</ul>
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+					<li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+	<body>
+		<div class="container">
+			<h1>Privacy Policy</h1>
+			Perslice USA Privacy Policy
+			<br>
+			<br>
+			This Privacy Policy describes how your personal information is collected, used, and shared when you visit or make a purchase from persliceusa.com (the “Site”).
+			This Privacy Policy describes how your personal information is collected, used, and shared when you visit or make a purchase from persliceusa.com (the “Site”).
+			<br>
+			<br>
+			PERSONAL INFORMATION WE COLLECT
+			When you visit the Site, we automatically collect certain information about your device, including information about your web browser, IP address, time zone, and some of the cookies that are installed on your device. Additionally, as you browse the Site, we collect information about the individual web pages or products that you view, what websites or search terms referred you to the Site, and information about how you interact with the Site. We refer to this automatically-collected information as “Device Information”.
+			<br>
+			<br>
+			We collect Device Information using the following technologies:
+			- “Cookies” are data files that are placed on your device or computer and often include an anonymous unique identifier. For more information about cookies, and how to disable cookies, visit http://www.allaboutcookies.org.
+			- “Log files” track actions occurring on the Site, and collect data including your IP address, browser type, Internet service provider, referring/exit pages, and date/time stamps.
+			- “Web beacons”, “tags”, and “pixels” are electronic files used to record information about how you browse the Site.
+			<br>
+			<br>
+			Additionally when you make a purchase or attempt to make a purchase through the Site, we collect certain information from you, including your name, billing address, shipping address, payment information (including credit card numbers, email address, and phone number. We refer to this information as “Order Information”.
+			<br>
+			<br>
+			When we talk about “Personal Information” in this Privacy Policy, we are talking both about Device Information and Order Information.
+			<br>
+			<br>
+			HOW DO WE USE YOUR PERSONAL INFORMATION?
+			<br>
+			<br>
+			We use the Order Information that we collect generally to fulfill any orders placed through the Site (including processing your payment information, arranging for shipping, and providing you with invoices and/or order confirmations). Additionally, we use this Order Information to:
+			- Communicate with you;
+			- Screen our orders for potential risk or fraud; and
+			- When in line with the preferences you have shared with us, provide you with information or advertising relating to our products or services.
+			We use the Device Information that we collect to help us screen for potential risk and fraud (in particular, your IP address), and more generally to improve and optimize our Site (for example, by generating analytics about how our customers browse and interact with the Site, and to assess the success of our marketing and advertising campaigns).
+			<br>
+			<br>
+			SHARING YOUR PERSONAL INFORMATION
+			<br>
+			<br>
+			We share your Personal Information with third parties to help us use your Personal Information, as described above. For example, we use Shopify to power our online store--you can read more about how Shopify uses your Personal Information here: https://www.shopify.com/legal/privacy. We also use Google Analytics to help us understand how our customers use the Site -- you can read more about how Google uses your Personal Information here: https://www.google.com/intl/en/policies/privacy/. You can also opt-out of Google Analytics here: https://tools.google.com/dlpage/gaoptout.
+			<br>
+			<br>
+			Finally, we may also share your Personal Information to comply with applicable laws and regulations, to respond to a subpoena, search warrant or other lawful request for information we receive, or to otherwise protect our rights.
+			<br>
+			<br>
+			BEHAVIOURAL ADVERTISING
+			<br>
+			<br>
+			As described above, we use your Personal Information to provide you with targeted advertisements or marketing communications we believe may be of interest to you. For more information about how targeted advertising works, you can visit the Network Advertising Initiative’s (“NAI”) educational page at http://www.networkadvertising.org/understanding-online-advertising/how-does-it-work.
+			<br>
+			<br>
+			You can opt out of targeted advertising by using the links below:
+			- Facebook: https://www.facebook.com/settings/?tab=ads
+			- Google: https://www.google.com/settings/ads/anonymous
+			- Bing: https://advertise.bingads.microsoft.com/en-us/resources/policies/personalized-ads
+			<br>
+			<br>
+			Additionally, you can opt out of some of these services by visiting the Digital Advertising Alliance’s opt-out portal at: http://optout.aboutads.info/.
+			<br>
+			<br>
+			DO NOT TRACK
+			<br>
+			<br>
+			Please note that we do not alter our Site’s data collection and use practices when we see a Do Not Track signal from your browser.
+			<br>
+			<br>
+			YOUR RIGHTS
+			<br>
+			<br>
+			If you are a European resident, you have the right to access personal information we hold about you and to ask that your personal information be corrected, updated, or deleted. If you would like to exercise this right, please contact us through the contact information below.
+			<br>
+			<br>
+			Additionally, if you are a European resident we note that we are processing your information in order to fulfill contracts we might have with you (for example if you make an order through the Site), or otherwise to pursue our legitimate business interests listed above. Additionally, please note that your information will be transferred outside of Europe, including to Canada and the United States.
+			<br>
+			<br>
+			DATA RETENTION
+			<br>
+			<br>
+			When you place an order through the Site, we will maintain your Order Information for our records unless and until you ask us to delete this information.
+			<br>
+			<br>
+			CHANGES
+			<br>
+			<br>
+			We may update this privacy policy from time to time in order to reflect, for example, changes to our practices or for other operational, legal or regulatory reasons.
+			<br>
+			<br>
+			MINORS
+			<br>
+			<br>
+			Please make sure your parents have authorized any purchases on The Site.
+			<br>
+			<br>
+			CONTACT US
+			<br>
+			<br>
+			For more information about our privacy practices, if you have questions, or if you would like to make a complaint, please contact us by e-mail at Help@persliceusa.com or by mail using the details provided below:
+			<br>
+			<br>
+			Perslice USA
+			[Re: Privacy Compliance Officer]
+
+		</div>
+	</body>
+	<?php include 'footer.php'?>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</html>
