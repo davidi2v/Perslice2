2891003aed4c2a3d891b939af583058603e2661b
David Juarez <davidjuarez1411@gmail.com>
2018-07-27 15:40:00 -0700

    added faq page with a search bar that searches for a question on the list. Used JavaScript for the search functionality of the search bar.

---
+++ faq.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Frequently Asked Questions</title>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/style.css">
+	</head>
+	<?php include 'banner.php'?>
+	<nav class="navbar">
+		<div class="container">
+			<a id="number" href="#">CALL NOW : (888) 567-1901</a>
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="#">
+					<img src="img/Logo_Perslice.png" alt="perslice logo">
+				</a>
+			</div>
+			<div class="collapse navbar-collapse" id="myNavbar">
+				<ul class="nav navbar-nav">
+					<li><a href="#">HOME</a></li>
+					<li class="active"><a href="shop.php">SHOP</a></li>
+					<li class="dropdown">
+						<a class="dropdown-toggle" data-toggle="dropdown" href="#">CONTACT <span class="caret"></span></a>
+						<ul class="dropdown-menu">
+						<li><a href="#">RETAILER</a></li>
+						<li><a href="#">DISTRIBUTOR</a></li>
+						</ul>
+					</li>
+					<li class="dropdown">
+						<a class="dropdown-toggle" data-toggle="dropdown" href="blogs.php">ARTICLES<span class="caret"></span></a>
+						<ul class="dropdown-menu">
+						<li><a href="blogs.php">BLOGS</a></li>
+						<li><a href="recipes-all.php">RECIPES</a></li>
+						<li><a href="#">SPECIAL EVENTS</a></li>
+						</ul>
+					</li>
+					<li><a href="#">HOW TO USE</a></li>
+				</ul>
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+					<li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+	<body>
+			<div class="container">
+				<h1>Frequently Asked Questions</h1>
+			</div>
+			<div id="search">
+				<input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search for names.." title="Type in a name">
+			</div>
+		<div class="container">
+			<table id="myTable">
+			  <tr class="header">
+			    <!-- <th style="width:60%;">Name</th> -->
+			  </tr>
+			  <tr>
+				<td>
+					<b>How many slices does Perslice cut?</b>
+					<p>The answer goes here.</p>
+				</td>
+			  </tr>
+			  <tr>
+				<td>
+					<b>What desserts can Perslice cut?</b>
+					<p>The answer goes here.</p>
+				</td>
+			  </tr>
+			  <tr>
+				<td>
+					<b>How do you clean the Perslice cake slicer?</b>
+					<p>The answer goes here.</p>
+				</td>
+			  </tr>
+			  <tr>
+				<td>
+					<b>What size cakes does Perslice slice?</b>
+					<p>The answer goes here.</p>
+				</td>
+			  </tr>
+			  <tr>
+				<td>
+					<b>Is Perlice a toy?</b>
+					<p>The answer goes here.</p>
+				</td>
+			  </tr>
+			</table>
+			<script>
+				function myFunction() {
+				  var input, filter, table, tr, td, i;
+				  input = document.getElementById("myInput");
+				  filter = input.value.toUpperCase();
+				  table = document.getElementById("myTable");
+				  tr = table.getElementsByTagName("tr");
+				  for (i = 0; i < tr.length; i++) {
+				    td = tr[i].getElementsByTagName("td")[0];
+				    if (td) {
+				      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+				        tr[i].style.display = "";
+				      } else {
+				        tr[i].style.display = "none";
+				      }
+				    }
+				  }
+				}
+			</script>
+		</div>
+	</body>
+	<?php include 'footer.php'?>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</html>
