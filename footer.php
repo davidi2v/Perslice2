af70cf7bcd3b917c05a84bb316e0a6abcbafda13
David Juarez <davidjuarez1411@gmail.com>
2018-08-03 11:21:36 -0700

    test

--- footer.php
+++ footer.php
@@ -22,14 +22,24 @@
 					<li><a href="press.php">Press</a></li>
 					<li><a href="all-events.php">Events</a></li>
 				</ul>
-				<ul id="socialFooter">
+				<!-- <ul id="socialFooter">
 					<li><a href="https://www.facebook.com/PersliceUSA/"><img src="img/fbIcon_blk.png"></a></li>
 					<li><a href="https://www.instagram.com/persliceusa/"><img src="img/igIcon.png"></a></li>
 					<li><a href="https://twitter.com/persliceusa?lang=en"><img src="img/twitterIcon.png"></a></li>
 					<li><a href="https://www.youtube.com/channel/UCNun0CgGBKZDRyAzXDZSYWg"><img src="img/youtubeIcon.png"></a></li>
-				</ul>
+				</ul> -->
 			</div>
-
+			<div id="newsletterForm" class="col-xs-12 col-md-4 text-center">
+				<p>Subscribe to our newsletter</p>
+				<form>
+				  <div class="form-group">
+					<label for="email" class="sr-only">Email address</label>
+					<input type="email" class="form-control" id="email" aria-describedby="email" placeholder="Email Address">
+					<!-- <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small> -->
+				  </div>
+				  <button type="submit" class="btn btn-primary">Subscribe Now</button>
+				</form>
+			</div>
 		</div>
 	</div>
 	<div id="Copyright">&copy; <?php echo date("Y"); ?> by Perslice</div>
