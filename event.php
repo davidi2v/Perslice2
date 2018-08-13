744dd73a9ba5df320975debfd9613fa717354d70
David Juarez <davidjuarez1411@gmail.com>
2018-08-13 12:05:35 -0700

    added confetti affect to navbar for all event related pages

--- event.php
+++ event.php
@@ -8,9 +8,10 @@
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
 		<link rel="stylesheet" href="css/style.css">
+		<link rel="stylesheet" href="css/confetti.css">
 	</head>
 	<?php include 'banner.php'?>
-	<nav class="navbar">
+	<nav class="navbar navbar-confetti">
 		<div class="container">
 			<a id="number" href="#">CALL : (888) 567-1901</a>
 			<div class="navbar-header">
@@ -151,4 +152,5 @@
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 	<script src="js/script.js"></script>
+	<script src="js/confetti.js"></script>
 </html>
