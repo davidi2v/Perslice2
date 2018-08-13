f9db5f3472857ada23f719990d72d00071c8a239
David Juarez <davidjuarez1411@gmail.com>
2018-08-13 12:47:09 -0700

    js update

--- js/script.js
+++ js/script.js
@@ -153,7 +153,7 @@ $(".input-number").keydown(function (e) {
 		var imagePos = $(this).offset().top;
 
 		var topOfWindow = $(window).scrollTop();
-			if (imagePos < topOfWindow+200) {
+			if (imagePos < topOfWindow+1200) {
 				$(this).addClass("slideUp");
 			}
 		});
