c065b4c7d96ab2e283de48b86782943c802ce20c
David Juarez <davidjuarez1411@gmail.com>
2018-08-14 13:10:35 -0700

    added phone anchro tag for mobile only to be clickable for autodial

--- index.php
+++ index.php
@@ -13,7 +13,8 @@
 	<?php include 'banner.php'?>
 	<nav class="navbar">
 		<div class="container">
-			<a id="number" href="#">CALL : (888) 567-1901</a>
+			<a id="numberMobile" href="tel:3177594940">CALL : (888) 567-1901</a>
+			<a id="number" href="">CALL : (888) 567-1901</a>
 			<div class="navbar-header">
 				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
 					<span class="icon-bar"></span>
